fe2949cb7a0c9499dda740faf35192e4a59b2138
Shrotofo <109517968+shrotofo@users.noreply.github.com>
2024-05-30 23:16:11 +0800

    Update and rename Problem1 to Problem1.tsx

--- Problem1.tsx
+++ Problem1.tsx
@@ -1,24 +1,25 @@
 //one way
-
 var sum_to_n_a = function(n) { 
-return (n * (n + 1)) / 2; 
+  return (n * (n + 1)) / 2; 
 }; 
+
 
 //two way
 var sum_to_n_b = function(n) {
   if (n <= 1) {
    return n;
-    } 
-else { return n + sum_to_n_c(n - 1);  
-} 
+  } 
+else { 
+  return n + sum_to_n_c(n - 1);  
+  } 
 };
 
-//third way
 
+//third way
 var sum_to_n_c = function(n) {
     let sum = 0;
     for (let i = 1; i <= n; i++) {
         sum += i;
-}
+    }
    return sum;   
 };
